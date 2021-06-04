d5ad68926f7cf9d7c686cfda346212b57f2fe32e
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-06-05 00:25:37 +0300

    Add articles file

--- config.py
+++ config.py
@@ -2,36 +2,17 @@ import os
 
 class Config:
 
-    
    	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
    	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
    	NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+   	@staticmethod
+   	def init_app(app):
+   		pass
+
 
 class ProdConfig(Config):
     pass
-import os
-class Config:
-    '''
-    General configuration parent class
-    '''
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
-class DevConfig(Config):
-    '''
-    Development  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    DEBUG = True
-config_options = {
-    'development':DevConfig,
-    'production':ProdConfig
-    }
+
 
 class DevConfig(Config):
     DEBUG = True
@@ -39,4 +20,5 @@ class DevConfig(Config):
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
+
 }
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-export NEWS_API_KEY='50ae1aea669a4f5b8c13504509145204'
+ export NEWS_API_KEY='50ae1aea669a4f5b8c13504509145204'
 
 python3.8 manage.py server
